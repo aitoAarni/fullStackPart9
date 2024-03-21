e1af4924790d46193595f280db9382d966ecbb04
Aarni <aarni.rechardt@gmail.com>
2024-03-21 23:59:25 +0200

    patientor different entry type creation added to UI

--- patientor/src/components/PatientView/CreateEntry.tsx
+++ patientor/src/components/PatientView/CreateEntry.tsx
@@ -7,20 +7,30 @@ const CreateEntry = () => {
   const [date, setDate] = useState("");
   const [specialist, setSpecialist] = useState("");
   const [diagnosisCodes, setDiagnosisCodes] = useState<string>("");
+  const [healthRating, setHealthRating] = useState("");
+  const [employerName, setEmployerName] = useState("");
+  const [sickLeave, setSickLeave] = useState("");
+  const [dischargeDate, setDischargeDate] = useState("");
+  const [dischargeCriteria, setDischargeCriteria] = useState("");
 
   const createEntry = () => {
     console.log(type);
   };
   return (
     <>
-      <ButtonGroup>
-        <Button onClick={() => setType("HealthCheck")}>health check</Button>
-        <Button onClick={() => setType("OccupationalHealthcare")}>
-          occupational healthcare
-        </Button>
-        <Button onClick={() => setType("Hospital")}>hospital</Button>
-      </ButtonGroup>
-      <form onSubmit={createEntry}>
+      <div>
+        <ButtonGroup>
+          <Button onClick={() => setType("HealthCheck")}>health check</Button>
+          <Button onClick={() => setType("OccupationalHealthcare")}>
+            occupational healthcare
+          </Button>
+          <Button onClick={() => setType("Hospital")}>hospital</Button>
+        </ButtonGroup>
+      </div>
+      <form
+        onSubmit={createEntry}
+        style={{ display: "flex", flexDirection: "column" }}
+      >
         <TextField
           label="Description"
           value={description}
@@ -42,7 +52,97 @@ const CreateEntry = () => {
           value={diagnosisCodes}
           onChange={(event) => setDiagnosisCodes(event.target.value)}
         />
+
+        {type === "HealthCheck" && (
+          <HealthEntry rating={healthRating} setRating={setHealthRating} />
+        )}
+        {type === "OccupationalHealthcare" && (
+          <OccupationalEntry
+            employerName={employerName}
+            setEmployerName={setEmployerName}
+            sickLeave={sickLeave}
+            setSickLeave={setSickLeave}
+          />
+        )}
+        {type === "Hospital" && (
+          <HospitalEntry
+            date={dischargeDate}
+            setDate={setDischargeDate}
+            criteria={dischargeCriteria}
+            setCriteria={setDischargeCriteria}
+          />
+        )}
       </form>
+    </>
+  );
+};
+
+const HealthEntry = ({
+  rating,
+  setRating,
+}: {
+  rating: string;
+  setRating: React.Dispatch<React.SetStateAction<string>>;
+}) => {
+  return (
+    <TextField
+      label="Health check rating"
+      value={rating}
+      onChange={(event) => setRating(event.target.value)}
+    />
+  );
+};
+
+const OccupationalEntry = ({
+  employerName,
+  setEmployerName,
+  sickLeave,
+  setSickLeave,
+}: {
+  employerName: string;
+  setEmployerName: React.Dispatch<React.SetStateAction<string>>;
+  sickLeave: string;
+  setSickLeave: React.Dispatch<React.SetStateAction<string>>;
+}) => {
+  return (
+    <>
+      <TextField
+        label="Employer name"
+        value={employerName}
+        onChange={(event) => setEmployerName(event.target.value)}
+      />
+      <TextField
+        label="Sick leave"
+        value={sickLeave}
+        onChange={(event) => setSickLeave(event.target.value)}
+      />
+    </>
+  );
+};
+
+const HospitalEntry = ({
+  date,
+  setDate,
+  criteria,
+  setCriteria,
+}: {
+  date: string;
+  setDate: React.Dispatch<React.SetStateAction<string>>;
+  criteria: string;
+  setCriteria: React.Dispatch<React.SetStateAction<string>>;
+}) => {
+  return (
+    <>
+      <TextField
+        label="Discharge date"
+        value={date}
+        onChange={(event) => setDate(event.target.value)}
+      />
+      <TextField
+        label="Discharge criteria"
+        value={criteria}
+        onChange={(eent) => setCriteria(eent.target.value)}
+      />
     </>
   );
 };
